502ff9974b8d4f6ec75fe827b4c39ee54a6ce166
krishnap1202 <163284353+krishnap1202@users.noreply.github.com>
2024-03-14 15:50:31 +0530

    Add files via upload
    Done

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+
+    alert("Username: " + username + "\nPassword: " + password);
+  });
+</script>
